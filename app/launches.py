346d0b8fc09de8c273a85d18e0425a12c199a0ed
CD-UNCC <Sclark91@uncc.edu>
2015-09-23 11:09:19 -0400

    work please

--- app/launches.py
+++ app/launches.py
@@ -1,6 +1,9 @@
 import json, urllib
-
+data = {}
 def get_launches():
-	results = json.load(urllib.urlopen("https://www.kimonolabs.com/api/ddi9jle8?apikey=a0OfY1qbs4Nn8a0cfkeuUzCOwOQzn1s8"))
-	data = results['results']['launches']
+	try:
+		results = json.load(urllib.urlopen("https://www.kimonolabs.com/api/ddi9jle8?apikey=a0OfY1qbs4Nn8a0cfkeuUzCOwOQzn1s8"))
+		data = results['results']['launches']
+	except:
+		data['date'] = 'try again later: sorry!'
 	return data
